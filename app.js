6f6b1d754f11d72f1e06783af32f6f7b84ab4bfd
Carlos Souza <carloshrsouza@gmail.com>
2014-11-19 10:15:27 -0500

    Add limit query string

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.use(express.static('public'));
 var blocks = [ 'Fixed', 'Movable', 'Rotating' ];
 
 app.get('/blocks', function(request, response) {
-  response.json(blocks);
+  // To test this with curl, run:
+  // curl http://localhost:3000/blocks?limit=2
+  if(request.query.limit >= 0){
+    response.json(blocks.slice(0, request.query.limit));
+  }else{
+    response.json(blocks);
+  }
 });
 
 app.listen(3000);
